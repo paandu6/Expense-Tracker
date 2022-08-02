7e36925aa8a48e3adfbc55db95c94e48c0c786cb
paandu6 <40341170+paandu6@users.noreply.github.com>
2022-08-02 19:07:07 +0530

    Used all edit,showonscreen,delte functions with

--- main.js
+++ main.js
@@ -11,39 +11,57 @@ function adding(e){
         cat : cat,
         expense : expense
     }
-    let objstr = JSON.stringify(obj)
-    localStorage.setItem(Description,objstr)
-    add(obj)
+    axios.post('https://crudcrud.com/api/307637b61d654f9491f2065a99ebf28d/expenses',obj)
+    .then((response)=>{
+        add(response.data)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 }
 
-function add(obj){
+function add(u){
     let parentNode = document.getElementById('items')
-    let childHTML = `<li id=${obj.Description}>${obj.expense} ${obj.cat} ${obj.Description}
-                    <button onclick = remove('${obj.Description}')>delete</button>
-                    <button onclick = edit('${obj.expense}','${obj.cat}','${obj.Description}')>Edit</button>
+    let childHTML = `<li id=${u._id}>${u.expense} ${u.cat} ${u.Description}
+                    <button onclick = remove('${u._id}')>delete</button>
+                    <button onclick = edit('${u.expense}','${u.cat}','${u.Description}')>Edit</button>
                     </li>`
     parentNode.innerHTML= parentNode.innerHTML + childHTML
 }
-// function add(obj){
-//     let parentNode = document.getElementById('items')
-//     const child = document.createElement('li')
-// }
-//deleting
-function remove(Description){
-    localStorage.removeItem(Description)
-    console.log(Description)
-    removefromscreen(Description)
+function remove(userid){
+    localStorage.removeItem(userid)
+    axios.delete(`https://crudcrud.com/api/307637b61d654f9491f2065a99ebf28d/expenses/${userid}`)
+    removefromscreen(userid)
 }
+//loading on screen
+
+window.addEventListener('DOMContentLoaded',()=>{
+
+    axios.get('https://crudcrud.com/api/307637b61d654f9491f2065a99ebf28d/expenses')
+            .then((response)=>{
+                for(let i=0;i<response.data.length;i++){
+                    add(response.data[i])
+                }
+            } )
+            .catch(err=>console.log(err))
+})
 //removing
-function removefromscreen(Description){
+function removefromscreen(userid){
     const parentNode = document.getElementById('items');
-    const childNode = document.getElementById(Description)
+    const childNode = document.getElementById(userid)
     parentNode.removeChild(childNode);
 }
 
 function edit(expense,cat,Description){
-    document.getElementById('expense').value=expense
-    document.getElementById('cat').value=cat
-    document.getElementById('description').value=Description
-    removefromscreen(Description)
+    document.getElementById('expense').value=expense;
+    document.getElementById('cat').value=cat;
+    document.getElementById('description').value = Description;
+    remove(userid)
+    axios.post(`https://crudcrud.com/api/307637b61d654f9491f2065a99ebf28d/expenses/${userid}`,{
+        fname : fname,
+        Phone : Phone,
+        emailid : emailid
+    })
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err))
 }
